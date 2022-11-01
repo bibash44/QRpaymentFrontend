224f50d1aa29f6061d9178da1f390024684c21fb
Bibash kattel <bibashkatel4@gmail.com>
2022-11-01 16:57:34 +0000

    hosting URL added

--- QRpayment/qr_pay/lib/services/BASEURL.dart
+++ QRpayment/qr_pay/lib/services/BASEURL.dart
@@ -3,7 +3,8 @@ import 'package:shared_preferences/shared_preferences.dart';
 class BASEURL {
   // String baseurl = 'http://localhost:51235/';
   // String baseurl = 'http://10.0.2.2:51235/';
-  String baseurl = 'http://192.168.0.21:51235/';
+  // String baseurl = 'http://192.168.0.21:51235/';
+  String baseurl = 'https://qrpay101.herokuapp.com/';
   // String baseurl = 'https://agendamemphis-remarkkarate-3000.codio-box.uk/';
   // String runFlutterWeb =flutter run -d chrome --web-renderer html --target lib/screens/splash_screen.dart;
 
